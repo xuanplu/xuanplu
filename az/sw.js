8e0435d30a7c09b5086efe356bb82cce7cb26e64
qianfen <343837867@qq.com>
2026-03-09 17:36:34 +0800

    Add files via upload

--- az/sw.js
+++ az/sw.js
@@ -1,21 +1,10 @@
-const CACHE_NAME = 'shixu-pwa-v2';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.js'
-];
+const CACHE_NAME = 'shixu-final-v1';
+const assets = ['./', './index.html', './manifest.json', 'https://cdn.jsdelivr.net/npm/lunar-javascript/lunar.js'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
